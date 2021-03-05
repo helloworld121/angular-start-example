5fcac71482b73f948692120863f29a88866bfd78
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-03-05 22:26:16 +0100

    basic: display shopping-list

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,12 +1,5 @@
 export class Recipe {
 
-  constructor(name: string, description: string, imagePath: string) {
-    this.name = name;
-    this.description = description;
-    this.imagePath = imagePath;
-  }
+  constructor(public name: string, public description: string, public imagePath: string) { }
 
-  public name: string;
-  public description: string;
-  public imagePath: string;
 }
