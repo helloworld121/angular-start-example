87e67bac792325e285e554c96a3d0e4e093155d3
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-03-11 16:33:13 +0100

    add child-routing for recipes and pass query parameters

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from '../../recipe.model';
-import {RecipeService} from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -12,13 +11,10 @@ export class RecipeItemComponent implements OnInit {
   @Input()
   recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  @Input()
+  index: number;
 
   ngOnInit(): void {
-  }
-
-  onSelected(): void {
-    this.recipeService.recipeSelected.emit(this.recipe);
   }
 
 }
