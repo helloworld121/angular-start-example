1d2597a8ee9c0f9cbf879d5efd775433c049db6f
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-03-17 21:26:45 +0100

    store data using HttpClient

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,11 +1,20 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {RecipeService} from '../recipes/recipe.service';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private recipeService: RecipeService) { }
+
+  storeRecipes(): void {
+    const recipes = this.recipeService.getRecipes();
+    this.httpClient.put(environment.baseUrl4Data + 'recipes.json', recipes).subscribe(response => {
+      console.log(response);
+    });
+  }
 
 }
