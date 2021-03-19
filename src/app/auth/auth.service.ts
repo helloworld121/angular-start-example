50ef34a3f0b21fb7bb719e771572692828c8f9a7
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-03-19 06:05:24 +0100

    authentication - handling http-errors in the backend using rxjs

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
-import {Observable} from 'rxjs';
+import {Observable, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
 
 
 interface AuthResponseData {
@@ -23,7 +24,22 @@ export class AuthService {
   signup(email: string, password: string): Observable<AuthResponseData> {
     return this.httpClient.post<AuthResponseData>(
       environment.baseUrl4SignUp + environment.firebaseApiKey,
-      {email, password, returnSecureToken: true});
+      {email, password, returnSecureToken: true}
+      ).pipe(
+        catchError(errorResponse => {
+          let errorMessage = 'An unknown error occured';
+          // it is possible, that there is no error-key => for example due to network problems
+          if (!errorResponse.error || errorResponse.error.error) {
+            return throwError(errorMessage);
+          }
+          switch (errorResponse.error.error.message) {
+            case 'EMAIL_EXISTS': errorMessage = 'The email address is already in use by another account.'; break;
+            case 'OPERATION_NOT_ALLOWED': errorMessage = 'Password sign-in is disabled for this project.'; break;
+            case 'TOO_MANY_ATTEMPTS_TRY_LATER': errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.'; break;
+          }
+          return throwError(errorMessage);
+        })
+      );
   }
 
 }
