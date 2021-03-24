ad92453f4c8553163c822b42a34e1fb132b36388
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-03-24 20:39:53 +0100

    modules - lazy loading using module-routes loadChildren => for auth and shopping-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,8 @@ import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
-import {RecipesModule} from './recipes/recipes.module';
-import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {SharedModule} from './shared/shared.module';
 import {CoreModule} from './core.module';
-import {AuthModule} from './auth/auth.module';
 
 @NgModule({
   // ATTENTION: multiple declarations of the same thing are not allowed
@@ -21,8 +18,6 @@ import {AuthModule} from './auth/auth.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    ShoppingListModule,
-    AuthModule,
     SharedModule,
     CoreModule,
   ],
