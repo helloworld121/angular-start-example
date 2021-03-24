e67c65f44a6866b7fba423b69af29cf4b227d1c1
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-03-24 20:19:27 +0100

    modules - extract auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
-import {AuthComponent} from './auth/auth.component';
 import {RecipesModule} from './recipes/recipes.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {SharedModule} from './shared/shared.module';
 import {CoreModule} from './core.module';
+import {AuthModule} from './auth/auth.module';
 
 @NgModule({
   // ATTENTION: multiple declarations of the same thing are not allowed
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
+    AuthModule,
     SharedModule,
     CoreModule,
   ],
