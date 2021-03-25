c39be208a6d73744badab54470df38865db9d2e7
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-03-25 16:26:42 +0100

    ngrx - rename actions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,12 +1,12 @@
 import { Action } from '@ngrx/store';
 import {Ingredient} from '../../shared/ingredient.model';
 
-export const ADD_INGREDIENT = 'ADD_INGREDIENT';
-export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
-export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
-export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
-export const START_EDIT = 'START_EDIT';
-export const STOP_EDIT = 'STOP_EDIT';
+export const ADD_INGREDIENT = '[Shopping List] add Ingredient';
+export const ADD_INGREDIENTS = '[Shopping List] add IngredientS';
+export const UPDATE_INGREDIENT = '[Shopping List] update Ingredient';
+export const DELETE_INGREDIENT = '[Shopping List] delete Ingredient';
+export const START_EDIT = '[Shopping List] start edit';
+export const STOP_EDIT = '[Shopping List] stop edit';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
