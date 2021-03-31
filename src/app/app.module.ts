031e047a41d9a08525102d42c1dfc5ea3143a816
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-03-31 14:02:15 +0200

    configure ngrx-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import {StoreModule} from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
 import {EffectsModule} from '@ngrx/effects';
 import {AuthEffects} from './auth/store/auth.effects';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
 
 @NgModule({
   // ATTENTION: multiple declarations of the same thing are not allowed
@@ -25,6 +27,7 @@ import {AuthEffects} from './auth/store/auth.effects';
     AppRoutingModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({logOnly: environment.production}),
     SharedModule,
     CoreModule,
   ],
